26aebe884eb126b0f11144724442fa6c196facd7
tianyw0 <hello.tianyongwei@gmail.com>
2025-07-13 16:36:49 +0800

    1

--- pages/content-ui/src/components/CollapseButton.tsx
+++ pages/content-ui/src/components/CollapseButton.tsx
@@ -16,14 +16,29 @@ export const CollapseButton: React.FC<CollapseButtonProps> = ({ expand, onToggle
       onClick={handleClick}
       className={cn('p-1 rounded flex items-center justify-center dark:hover:bg-gray-700 hover:bg-gray-200')}
       title={expand ? t('expand') : t('collapse')}>
-      <svg
-        className={cn('w-4 h-4 transform transition-transform', !expand && 'rotate-180')}
-        viewBox='0 0 24 24'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='2'>
-        <path d='M15 19l-7-7 7-7' />
-      </svg>
+      {expand ? (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          className='bi bi-layout-sidebar-inset-reverse font-bold'
+          viewBox='0 0 16 16'>
+          <path d='M2 2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h12z' />
+          <path d='M13 4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V4z' />
+        </svg>
+      ) : (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          className='bi bi-layout-sidebar-inset text-gray-800'
+          viewBox='0 0 16 16'>
+          <path d='M14 2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z' />
+          <path d='M3 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z' />
+        </svg>
+      )}
     </button>
   );
 };
