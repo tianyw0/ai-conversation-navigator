44d6193cb9b16eda6331453c477f5dd9bec517ae
tianyw0 <hello.tianyongwei@gmail.com>
2025-07-19 17:37:01 +0800

    theme trigger need run one time first

--- pages/content-ui/src/trigger/themeChangeTrigger.ts
+++ pages/content-ui/src/trigger/themeChangeTrigger.ts
@@ -1,11 +1,16 @@
 export function themeChangeTrigger() {
-  const themeObserver = new MutationObserver(() => {
+  function monitor() {
     const newTheme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
     const mountPoint = document
       .querySelector('#ai-conversation-navigator-root')
       ?.shadowRoot?.querySelector('#mount-point');
     mountPoint && (mountPoint.className = newTheme);
     console.log(`new theme is ${newTheme}`);
+  }
+  // 先运行一次
+  monitor();
+  const themeObserver = new MutationObserver(() => {
+    monitor();
   });
 
   themeObserver.observe(document.documentElement, {
